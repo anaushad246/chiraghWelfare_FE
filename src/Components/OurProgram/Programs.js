b3fe18519221addd798aa5874eb9791d442f35c8
anaushad246 <anaushad246@gmail.com>
2024-08-23 14:38:42 +0530

    changes in images path

--- src/Components/OurProgram/Programs.js
+++ src/Components/OurProgram/Programs.js
@@ -2,7 +2,7 @@ const programs = [
     {
       id: 1,
       name: 'Kalam Scholarship',
-      image: '/src/assets/kalam-scholarship.jpg',
+      image: '/kalam-scholarship.jpg',
       content: `This scholarship aims to support students in their pursuit of higher education and provide them direct enrollment in several colleges and universities throughout India. Selections are made based on factors such as annual family income and academic performance. This scholarship also supports students who are pursuing higher education. College and graduate school can be very expensive, so this scholarship is meant to help students afford to continue their studies after finishing high school. Under this scholarship, there are several programs prepared by various institutions across the country that provide this scholarship to Indian students.`,
     },
     {
@@ -20,43 +20,43 @@ const programs = [
     {
       id: 4,
       name: 'Health Care',
-      image: '/src/assets/HealthCare.jpg',
+      image: '/HealthCare.jpg',
       content: `We provide holistic healthcare to the rural poor of our communities. We have camped several times for primary healthcare, offering high-quality and low-cost treatment, operating across the state/territories. We also conduct door-to-door screenings and free medical camps in rural areas for early detection and treatment of illnesses.`,
     },
     {
       id: 5,
       name: 'Home Industries',
-      image: '/src/assets/HomeIndustries.jpg',
+      image: '/HomeIndustries.jpg',
       content: `We offer opportunities to the underprivileged, especially in rural areas, by establishing home industries that provide women and skilled persons with income-generating opportunities, fostering economic independence. Women or poor persons develop and enhance valuable skills like crafting and product design through home-based work.`,
     },
     {
       id: 6,
       name: 'Hand Pump Project',
-      image: '/src/assets/HandPump.jpg',
+      image: '/HandPump.jpg',
       content: `The objective of the project is to provide safe drinking water to villagers. Water is a fundamental human need, and an average human being requires 20 to 50 liters of clean & safe water every day for drinking, cooking, etc. Presently, they are fetching water from very faraway places. The installation of hand pumps will benefit villagers in meeting their basic need for water. Together, we can secure safer water sources. We focus on the most affected areas to tackle water poverty by providing people with crucial amenities like hand pumps. We believe that everyone should be able to access clean water, and this access underpins all other aspects of development. These initiatives have gone a long way in improving the lives of many people across India.`,
     },
     {
       id: 7,
       name: 'Vocational Training',
-      image: '/src/assets/VocationalTraining.jpg',
+      image: '/VocationalTraining.jpg',
       content: `Vocational training focuses on developing technical skills for a specific job or trade. It offers practical knowledge in contrast to the theoretical knowledge offered by the conventional formal education system.`,
     },
     {
       id: 8,
       name: 'Disaster Project',
-      image: '/src/assets/Disaster.jpg',
+      image: '/Disaster.jpg',
       content: `A disaster project is a strategy created to aid a community or organization in disaster prevention, response, and recovery. Natural or man-made, disasters can result in a variety of harm, including the destruction of physical property, injuries, floods, earthquakes, and fatalities.`,
     },
     {
       id: 9,
       name: 'Helping Hand',
-      image: '/src/assets/HelpingHand.jpg',
+      image: '/HelpingHand.jpg',
       content: `Under this project, we consider financial burdens or even inauspicious circumstances, such as widows being relegated to the fringe of society. Our team always reflects love for the poor and needy by sponsoring various welfare activities for them, such as providing blankets, food kits, and basic expenses for widows.`,
     },
     {
       id: 10,
       name: 'Orphan Scholarship',
-      image: '/src/assets/OrphanScholarship.jpg',
+      image: '/OrphanScholarship.jpg',
       content: `We offer educational aid to students who are parentless for any educational purpose, such as tuition fees, examination fees, textbooks, or any required equipment. We aim to give them a chance to exercise their educational rights because, in this rapidly changing world, education is the key that unlocks life’s great opportunities.`,
     },
   ];

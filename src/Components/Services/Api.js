7c6f1c48bc590085f00e99252e4fe88d81b245f9
anaushad246 <anaushad246@gmail.com>
2024-10-27 19:32:41 +0530

    some issue fixed

--- src/Components/Services/Api.js
+++ src/Components/Services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/api/visitor';
+
 
 export const fetchVisitorCounts = async () => {
-  const response = await axios.post(`${API_URL}/increment-visitor`);
+  const response = await axios.post(`https://chiraghwelfare-be.onrender.com/api/visitor/increment-visitor`);
   console.log(response.data);
   return response.data; // Make sure to return the data
 };
